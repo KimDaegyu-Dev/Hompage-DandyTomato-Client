0e0b3c35c5e3a54ca3105bceb54c264a06913082
KimDaegyu <136952602+KimDaegyu-Dev@users.noreply.github.com>
2025-07-03 19:16:18 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@
 import { createRoot } from 'react-dom/client'
 import './main.css'
 import App from './App'
-import { worker } from './mocks/browser'
+// import { worker } from './mocks/browser'
 import { ThemeProvider } from './components/theme-provider'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
 
 const queryClient = new QueryClient()
-if (import.meta.env.MODE === 'development') {
-    worker.start()
-}
+// if (import.meta.env.MODE === 'development') {
+// worker.start()
+// }
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
